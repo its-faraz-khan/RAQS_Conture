b0a3ddd458baf31746b9a44c9b7acd58ba8f37e2
Muhammad Faraz Munir Khan <farazkhan4861@gmail.com>
2026-02-27 06:13:42 +0500

    first commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,33 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//     },
-//     cartData: {
-//       type: Object,
-//       default: {},
-//     },
-//   },
-//   { minimize: false }
-// );
-
-// const userModel = mongoose.models.user || mongoose.model("user", userSchema);
-
-// export default userModel;
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
